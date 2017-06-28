449dbb74a30743b251c953e0383cf30ba7e83aa2
Pascal Huerst <pascal.huerst@gmail.com>
2017-06-28 14:55:29 +0200

    nepos: Add nepos style

---
+++ src/virtualkeyboard/content/styles/nepos/images/adapt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mv Keyboard-Button_delete.svg			btn_delete.svg
+mv Keyboard-Button_functions.svg		btn_functions.svg
+mv Keyboard-Button_main.svg			btn_main.svg
+mv Keyboard-Button-return.svg			btn_return.svg
+mv Keyboard-Button_shift.svg			btn_shift.svg
+mv Keyboard-Button_switch-function.svg		btn_switch_function.svg
